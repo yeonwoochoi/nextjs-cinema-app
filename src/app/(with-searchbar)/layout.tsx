ed0e84ec1e60d4fdbeb239b5d2f606a514815733
beagle <chldusdn20@gmail.com>
2025-07-04 20:03:26 +0900

    feat: add search bar component and add it in layout

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,9 +1,12 @@
 import {ReactNode} from "react";
+import Searchbar from "../../components/searchbar";
 
 export default function SearchLayout ({children}: {children: ReactNode}) {
   return (
     <>
-      <div>Searchbar Layout</div>
+      <div>
+        <Searchbar />
+      </div>
       <div>{children}</div>
     </>
   )
